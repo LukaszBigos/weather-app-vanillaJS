61d0ef718fe4505451f058a23d641e8fc7013fc7
enduroRider <luki.bigos@gmail.com>
2019-04-14 12:22:58 +0200

    progress on fetching data

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-const searchBtn = document.getElementById('search-btn');
+const searchBtn = document.getElementById("search-btn");
 
-const appId = '059bb4f68a529c6aa7a74e55b92e3cfd';
-const units = 'metric';
-const searchMethod = 'city';
+const appId = "059bb4f68a529c6aa7a74e55b92e3cfd";
+const units = "metric";
+let searchMethod;
 
+searchBtn.addEventListener("click", () => {
+  const searchTerm = document.getElementById("search-input").value;
+  if (searchTerm) {
+    searchWeather(searchTerm);
+  }
+});
 
-const searchWeather = (searchTerm) => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&APPID=${appId}&units=${units}`)
+const searchWeather = searchTerm => {
+  getSearchMethod(searchTerm);
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`
+  )
     .then(result => {
       return result.json();
     })
@@ -15,13 +24,50 @@ const searchWeather = (searchTerm) => {
     });
 };
 
-const init = (resultFromServer) => {
-  console.log(resultFromServer);
+const getSearchMethod = searchTerm => {
+  if (searchTerm.length === 5 && !isNaN(searchTerm)) {
+    searchMethod = "zip";
+  } else {
+    searchMethod = "q";
+  }
 };
 
-searchBtn.addEventListener('click', () => {
-  const searchTerm = document.getElementById('search-input').value;
-  if (searchTerm) {
-    searchWeather(searchTerm);
+const init = resultFromServer => {
+  console.log(resultFromServer)
+  switch (resultFromServer.weather[0].main) {
+    case 'Clear':
+      document.body.style.backgroundImage = 'url("../assets/clear-sky.jpg")';
+      break;
+    case 'Clouds':
+      document.body.style.backgroundImage = 'url("../assets/clouds.jpg")';
+      break;
+    case 'Rain':
+    case 'Drizzle':
+    case 'Mist':
+      document.body.style.backgroundImage = 'url("../assets/rain.jpg")';
+      break;
+    case 'Thunderstorms':
+      document.body.style.backgroundImage = 'url("../assets/storm.jpg")';
+      break;
+    case 'Snow':
+      document.body.style.backgroundImage = 'url("../assets/snow.jpg")';
+      break;
+    default:
+      document.body.style.backgroundImage = 'url("../assets/default.jpg")';
+      break;
   }
-});
+
+  const cityHeader = document.getElementById('city-header');
+  const temperature = document.getElementById('temperature');
+  const weatherDescriptionHeader = document.getElementById('weather-description-header');
+  const weatherIcon = document.getElementById('weather-summary-icon');
+  const wind = document.getElementById('wind-speed');
+  const humidity = document.getElementById('humidity');
+
+  weatherDescriptionHeader.innerHTML = resultFromServer.weather[0].description;
+  cityHeader.innerHTML = resultFromServer.name;
+  temperature.innerHTML = `${Math.floor(resultFromServer.main.temp)} &#176C`;
+  weatherIcon.src = `http://openweathermap.org/img/w/${resultFromServer.weather[0].icon}.png`;
+  wind.innerHTML = `wind: ${Math.floor(resultFromServer.wind.speed)} m/s`;
+  humidity.innerHTML = `humidity: ${resultFromServer.main.humidity} %`;
+};
